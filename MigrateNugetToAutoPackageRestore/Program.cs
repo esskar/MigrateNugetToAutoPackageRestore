4e9214542b91bfa9358c2aa3dad40e8a77d72ff1
esskar <sk@intertivity.com>
2014-07-21 10:55:54 +0200

    integrate commdline library

--- MigrateNugetToAutoPackageRestore/Program.cs
+++ MigrateNugetToAutoPackageRestore/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using CommandLine;
+using CommandLine.Text;
 
 namespace MigrateNugetToAutoPackageRestore
 {
@@ -11,11 +15,62 @@ namespace MigrateNugetToAutoPackageRestore
 
         static void Main(string[] args)
         {
-            Console.WriteLine("WARNING. This program will modify your solution and project files. Please make a backup before.");
-            Console.ReadLine();
+            var options = new Options();
+            if (!Parser.Default.ParseArguments(args, options)) 
+                return;
 
+            Console.WriteLine("WARNING. This program will modify your solution and project files. Please backup before continue.");
+            Console.ReadLine();
+           
             var migrate = new Migrate();
-            migrate.Start(args[0]);
+            migrate.Start(options.SolutionFolder);
         }
-    }
+
+        class Options
+        {
+            [Option('f', "folder", HelpText = "Folder containing the solution to migrate.", Required = true)]
+            public string SolutionFolder { get; set; }
+
+            [HelpOption('h', "help")]
+            public string GetUsage()
+            {
+                var assembly = Assembly.GetEntryAssembly();
+                var version = assembly.GetName().Version;
+                var help = new HelpText
+                {
+                    Heading = new HeadingInfo("MigrateNugetToAutoPackageRestore", version.ToString(3)),
+                    Copyright = new CopyrightInfo("Sascha Kiefer (esskar)", GetCopyrightYears()),
+                    AdditionalNewLineAfterOption = false,
+                    AddDashesToOption = true
+                };
+                help.AddPreOptionsLine(string.Format("Version: {0}", version));
+                help.AddPreOptionsLine("All rights reserved.");
+                help.AddPreOptionsLine(string.Empty);
+                help.AddPreOptionsLine(string.Format("Usage: {0} [options]", ExecutableName));
+                help.AddOptions(this);
+
+                return help;
+            }
+
+            private static string ExecutableName
+            {
+                get { return AppDomain.CurrentDomain.FriendlyName.ToLowerInvariant(); }
+            }
+
+            private static int[] GetCopyrightYears()
+            {
+                var year = 2014;
+                var now = DateTime.Now;
+
+                var years = new List<int>();
+                while (year <= now.Year)
+                {
+                    years.Add(year++);
+                }
+
+                return years.ToArray();
+            }
+        }
+
+    }    
 }
