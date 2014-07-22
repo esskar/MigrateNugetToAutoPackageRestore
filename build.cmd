20f401aa9a5780ca27bb3aecc8470141d8883229
esskar <sk@intertivity.com>
2014-07-22 09:12:14 +0200

    fix nuget restore

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 @echo off
 ".nuget\NuGet.exe" install -OutputDirectory packages .\packages.config
-".nuget\NuGet.exe" restore -OutputDirectory MigrateNugetToAutoPackageRestore\packages .\MigrateNugetToAutoPackageRestore\packages.config
+".nuget\NuGet.exe" restore MigrateNugetToAutoPackageRestore.sln
 "packages\Sake.0.2\tools\sake.exe" %*
